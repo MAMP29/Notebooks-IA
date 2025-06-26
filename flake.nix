cfba0914f189ce3b9127fbf0e3ecbcba94160e5e
MAMP29 <106500061+MAMP29@users.noreply.github.com>
2025-06-25 23:14:22 -0500

    notebook de EDA y red neuronal añadido

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Informe de IA";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        pythonEnv = pkgs.python313.withPackages (ps: with ps; [
+          jupyterlab
+          pandas
+          scikit-learn
+          matplotlib
+          seaborn
+          numpy
+        ]);
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+           pythonEnv
+           pkgs.zsh
+           pkgs.oh-my-posh
+          ];
+
+          shellHook = ''
+            echo "Entorno listo, recuerda crear un venv en python antes"
+
+            if [ -n "$ZSH_VERSION" ]; then
+              echo "Zsh detectado"
+            else
+              echo "Forzando Zsh..."
+              exec zsh
+            fi
+          '';
+        };
+      });
+
+}
